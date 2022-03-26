8c32699f550b19a06b86d4b53a5b399378f2a9df
59939 <59939@etu.he2b.be>
2022-03-26 19:19:59 +0100

    a continuer

---
+++ javascript/js-imc.js
@@ -0,0 +1,10 @@
+let imc = ["moins de 16.5", "16,5 a 18,5", "18,5 a 25", "25 a 30", "30 a 35", "35 a 40", "40 et plus"];
+let description = ["denutrition", "maigreur", "poids normal", "surpoids", "obesite modree", "obesite severe", "obesite morbide ou massive"]
+
+$(function() {
+    for(let i = 0 ; i < imc.length ; i++){
+        $(".tableBody").append($("<tr>"));
+        $(".tableBody tr:last-child").append($("<td>").text(imc[i]));
+        $(".tableBody tr:last-child").append($("<td>").text(description[i]));
+    }
+});
