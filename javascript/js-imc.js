d4f06b8c347520e84e30f83e2604743482748565
59939 <59939@etu.he2b.be>
2022-03-29 23:48:02 +0200

    3.5 page imc : ajoute un tableau dynamique

--- javascript/js-imc.js
+++ javascript/js-imc.js
@@ -1,14 +1,9 @@
 /**
  * Tableaux de donnees global
- * tabImc
- * intervallesImc
- * escription
- * intervallesPoids
  */
 let tabImc = [16.5, 18.5, 25, 30, 35, 40];
-let intervallesImc = ["moins de 16.5", "16,5 a 18,5", "18,5 a 25", "25 a 30", "30 a 35", "35 a 40", "40 et plus"];
-let description = ["denutrition", "maigreur", "poids normal", "surpoids", "obesite modree", "obesite severe", "obesite morbide ou massive"]
-let intervallesPoids = [];
+let tabDescription = ["denutrition", "maigreur", "poids normal", "surpoids", "obesite modree", "obesite severe", "obesite morbide ou massive"]
+let tabIntervallesPoids = [];
 
 /**
  * this function use description[]Array to give the user a description of his/her IMC
@@ -19,7 +14,7 @@ function imcDescription(imcUser) {
     let resultat = "obesite morbide ou massive";
     for (let i = 0; i < tabImc.length; i++) {
         if (imcUser < tabImc[i]) {
-            resultat = description[i];
+            resultat = tabDescription[i];
             i = tabImc.length;
         }
     }
@@ -33,33 +28,42 @@ function imcDescription(imcUser) {
  * @returns IMC the body mass index
  */
 function calculerImc(p, t) {
-    /**
-     * Rounding imc to 1 digit after comma.
-     * toFixed() - method converts a number into a string, keeping a specified number of decimals.It does not 
-     * actually rounds up a number, it truncates the number.
-     * Math.round(n) - rounds a number to the nearest integer.
-     */
     const imc = (Math.round((t / (p * p)) * 100) / 100).toFixed(1);
     return imc;
 }
 
 /**
- * this function creates 'in html' the Second Table with Poids and Description
+ * this function shows all the tables
+ * @param {*} quelTableau 1 fo the first & 2 for the dynamic table
+ * @param {*} colonne1DuTableau data of the first column "IMC ou Poids"
  */
-function tabDynamique() {
-     //this instruction generate the head of the second table (poids/description)
-     $(".tableBody2").before((`<thead class="enteteTab2"><tr><th>Poids</th><th>Description</th></tr></thead>`));
+function afficherTableau(quelTableau, colonne1DuTableau, imc) {
 
-    for (let i = 0; i < intervallesImc.length; i++) {
-        $(".tableBody2").append($("<tr>"));
-        $(".tableBody2 tr:last-child").append($("<td>").text(intervallesPoids[i]));
-        $(".tableBody2 tr:last-child").append($("<td>").text(description[i]));
-    };
+    for (let i = 1; i <= tabDescription.length; i++) {
+        if (i === 1) {
+            $(`.tableBody${quelTableau}`).append($("<tr>"));
+            $(`.tableBody${quelTableau} tr:last-child`).append($("<td>").text(`moins de ${colonne1DuTableau[i - 1]}`));
+            $(`.tableBody${quelTableau} tr:last-child`).append($("<td>").text(tabDescription[i - 1]));
+        } else if (i === tabDescription.length) {
+            $(`.tableBody${quelTableau}`).append($("<tr>"));
+            $(`.tableBody${quelTableau} tr:last-child`).append($("<td>").text(`${colonne1DuTableau[i - 2]}  et plus`));
+            $(`.tableBody${quelTableau} tr:last-child`).append($("<td>").text(`${tabDescription[i - 1]}`));
+        } else {
+            $(`.tableBody${quelTableau}`).append($("<tr>"));
+            $(`.tableBody${quelTableau} tr:last-child`).append($("<td>").text(`${colonne1DuTableau[i - 2]} a ${colonne1DuTableau[i - 1]}`));
+            $(`.tableBody${quelTableau} tr:last-child`).append($("<td>").text(tabDescription[i - 1]));
+        }
+    }
+    $(`.tableBody2 tr:nth-child(${tabEvidence(imc)})`).addClass("categorie-en-evidence");
 }
 
-function genererIntervallesPoids (taille) {
-    for(let i = 0 ; i < description.length ; i++) {
-        intervallesPoids[i] = (Math.round(((tabImc[i] * (taille * taille)) * 100)/100));
+/**
+ * Set tabIntervallesPoids data
+ * @param {*} taille User's height 
+ */
+function generertabIntervallesPoids(taille) {
+    for (let i = 0; i < tabDescription.length; i++) {
+        tabIntervallesPoids[i] = (Math.round(((tabImc[i] * (taille * taille)) * 100) / 100));
     }
 }
 
@@ -81,22 +85,12 @@ function tabEvidence(imcEvidence) {
 }
 
 /**
- * this function creates 'in html' the First Table with IMC and Description
- */
-function tabImcDescription() {
-    for (let i = 0; i < intervallesImc.length; i++) {
-        $(".tableBody").append($("<tr>"));
-        $(".tableBody tr:last-child").append($("<td>").text(intervallesImc[i]));
-        $(".tableBody tr:last-child").append($("<td>").text(description[i]));
-    };
-}
-
-/**
  * This function get all data from Browser's local storage and show it in html
  */
 function getAllStorage() {
+    keys = Object.keys(localStorage);
     for (let i = 0; i < keys.length; i++) {
-        $("#list-historique").append($("<p>").text(('IMC ' + (i + 1)) + " / " + (localStorage.getItem('IMC ' + (i + 1) ) )));
+        $("#list-historique").append($("<p>").text(('IMC ' + (i + 1)) + " / " + (localStorage.getItem('IMC ' + (i + 1)))));
     };
 }
 
@@ -116,97 +110,61 @@ function time() {
     return time;
 }
 
-
-
 //instructions to set i equal to localStorage,length
 keys = Object.keys(localStorage);
 i = keys.length;
 
-/**
- * Jquery Document ready function
- */
 $(function () {
-    //First table (IMC/Description)
-    tabImcDescription();
 
-    /**
-     * this function treats the 'calcul & enregistrer' buttons behavior
-     */
+    afficherTableau(1, tabImc);
+
     $("#calcul").on("click", function () {
-
         $(".enteteTab2").remove();
         $(".tableBody2 > *").remove();
-
         let taille = $("#taille").val() / 100;
         let poids = $("#poids").val();
-
         let imc = calculerImc(taille, poids);
-
-        genererIntervallesPoids(taille);
-
-        //this instruction generate the paragraph in calculatrice tab
+        generertabIntervallesPoids(taille);
         $(".afficheImc-paragraph").text(`Avec votre taille (${taille}m)  et votre poide (${poids})kg, votre IMC est de ${imc} et est considere comme ${imcDescription(imc)}. Avec votre taille, voici votre categorie d'IMC en fonction de votre poids:`);
-
-        //Show the Second table (poids/Description)
-        tabDynamique();
-
-        $(`.tableBody2 tr:nth-child(${tabEvidence(imc)})`).addClass("categorie-en-evidence");
-
+        afficherTableau(2, tabIntervallesPoids, imc);
     });
 
     $("#enregistrer-button").on("click", function () {
-
         let taille = $("#taille").val() / 100;
         let poids = $("#poids").val();
-
         let imc = calculerImc(taille, poids);
-
         //increment local storage key position
         i++;
-
         localStorage.setItem(`IMC ${i}`, `Date: ${time()} / IMC: ${imc}`);
     });
-
     $("#historique-button").on("click", function () {
-
-
         if ($(this).attr('data-click-state') == 0) {
-            // Set data-click-state attribute to 0
+            // Make historique-button ready to for user click
             $(this).attr('data-click-state', 1);
-
-            //instruction to remove history from the web page if already existed
-            $("#historique > *").remove();
+            $("#list-historique > *").remove();
             $("#supprimer-tout").remove();
-
+            getAllStorage().delay(800);
             if (localStorage.length <= 0) {
                 alert("Aucun Historique");
                 $("#historique-button").attr('data-click-state', 0);
             };
-            //generate supprimer-tout button with localStorage if data exist in local.Storage
             if (localStorage.length > 0) {
-                $("#historique").before(`<button id="supprimer-tout" type="button" class="btn btn-outline-primary">supprimer-tout</button>`);
+                $("#list-historique").before(`<button id="supprimer-tout" type="button" class="btn btn-outline-primary">supprimer-tout</button>`);
             }
-
             $("#supprimer-tout").on("click", function () {
-                //clear local storage
                 if (localStorage.length > 0) {
                     localStorage.clear();
                 }
-
-                //to remove history from web
-                $("#historique > *").remove();
+                $("#list-historique > *").remove();
                 $("#supprimer-tout").remove();
                 location.reload();
                 // Make historique-button ready to for user click
                 $("#historique-button").attr('data-click-state', 1);
             });
-
-            getAllStorage().delay(800);
         }
         else {
             $(this).attr('data-click-state', 0);
-
-            $("#historique > *").remove();
+            $("#list-historique > *").remove();
             $("#supprimer-tout").remove();
         }
     });
