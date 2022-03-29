85d103fcf2d65f9d4b26f9b947a10fac274ab4b2
59939 <59939@etu.he2b.be>
2022-03-29 21:56:23 +0200

    a continuer

--- javascript/js-imc.js
+++ javascript/js-imc.js
@@ -8,7 +8,7 @@
 let tabImc = [16.5, 18.5, 25, 30, 35, 40];
 let intervallesImc = ["moins de 16.5", "16,5 a 18,5", "18,5 a 25", "25 a 30", "30 a 35", "35 a 40", "40 et plus"];
 let description = ["denutrition", "maigreur", "poids normal", "surpoids", "obesite modree", "obesite severe", "obesite morbide ou massive"]
-let intervallesPoids = ["moins de 47", "47 a 53", "53 a 72", "72 a 86", "86 a 101", "101 a 115", "115 et plus"];
+let intervallesPoids = [];
 
 /**
  * this function use description[]Array to give the user a description of his/her IMC
@@ -47,11 +47,20 @@ function calculerImc(p, t) {
  * this function creates 'in html' the Second Table with Poids and Description
  */
 function tabDynamique() {
+     //this instruction generate the head of the second table (poids/description)
+     $(".tableBody2").before((`<thead class="enteteTab2"><tr><th>Poids</th><th>Description</th></tr></thead>`));
+
     for (let i = 0; i < intervallesImc.length; i++) {
         $(".tableBody2").append($("<tr>"));
         $(".tableBody2 tr:last-child").append($("<td>").text(intervallesPoids[i]));
         $(".tableBody2 tr:last-child").append($("<td>").text(description[i]));
     };
+}
+
+function genererIntervallesPoids (taille) {
+    for(let i = 0 ; i < description.length ; i++) {
+        intervallesPoids[i] = (Math.round(((tabImc[i] * (taille * taille)) * 100)/100));
+    }
 }
 
 /**
@@ -85,17 +94,10 @@ function tabImcDescription() {
 /**
  * This function get all data from Browser's local storage and show it in html
  */
-function allStorage() {
-
-    let values = [],
-        keys = Object.keys(localStorage);
-
-    for(let i = 0 ; i < keys.length ; i++) {
-        values[i] = (localStorage.getItem(keys[i]));
-    }
+function getAllStorage() {
     for (let i = 0; i < keys.length; i++) {
-        $("#historique").append($("<p>").text((i + 1)+ " / "+ keys[i] + " / " + values[i]));
-    }
+        $("#list-historique").append($("<p>").text(('IMC ' + (i + 1)) + " / " + (localStorage.getItem('IMC ' + (i + 1) ) )));
+    };
 }
 
 /**
@@ -106,7 +108,7 @@ function time() {
     let date = new Date();
     let year = date.getFullYear();
     let day = date.getDate();
-    let month = date.getMonth() + 1
+    let month = (date.getMonth() + 1);
     let seconds = date.getSeconds();
     let minutes = date.getMinutes();
     let hour = date.getHours();
@@ -117,9 +119,8 @@ function time() {
 
 
 //instructions to set i equal to localStorage,length
-let values = [],
-    keys = Object.keys(localStorage),
-    i = keys.length;
+keys = Object.keys(localStorage);
+i = keys.length;
 
 /**
  * Jquery Document ready function
@@ -132,84 +133,79 @@ $(function () {
      * this function treats the 'calcul & enregistrer' buttons behavior
      */
     $("#calcul").on("click", function () {
-        //those two instructions remove the second table (Poids/descirpton) if alreaady exist
+
         $(".enteteTab2").remove();
         $(".tableBody2 > *").remove();
 
-        //instructions to get user's taille end poids
         let taille = $("#taille").val() / 100;
         let poids = $("#poids").val();
 
-        // alert(`votre taille est : ${taille} m`);
-
-        //calculate user's IMC/BMI
         let imc = calculerImc(taille, poids);
 
-        // if condition to make user set a reasonable value
-        if (taille > 0.2 && poids > 5) {
+        genererIntervallesPoids(taille);
 
-            //this instruction generate the head of the second table (poids/description)
-            $(".tableBody2").before((`<thead class="enteteTab2"><tr><th>Poids</th><th>Description</th></tr></thead>`));
+        //this instruction generate the paragraph in calculatrice tab
+        $(".afficheImc-paragraph").text(`Avec votre taille (${taille}m)  et votre poide (${poids})kg, votre IMC est de ${imc} et est considere comme ${imcDescription(imc)}. Avec votre taille, voici votre categorie d'IMC en fonction de votre poids:`);
 
-            //this instruction generate the paragraph in calculatrice tab
-            $(".afficheImc").text(`Avec votre taille (${taille}m)  et votre poide (${poids})kg, votre IMC est de ${imc} et est considere comme ${imcDescription(imc)}`);
+        //Show the Second table (poids/Description)
+        tabDynamique();
 
-            //Show the Second table (poids/Description)
-            tabDynamique();
+        $(`.tableBody2 tr:nth-child(${tabEvidence(imc)})`).addClass("categorie-en-evidence");
 
-            //enregistrer button to save calcul result in the Browser LocalStorage
-            $("#enregistrer-button").on("click", function () {
-                i++;
-                localStorage.setItem('IMC ' + i, "Date: " + time() + " / IMC: " + imc);
-                $("#calcul").hide();
-                
-            });
-            //change background color of the table row using a Css class with 800ms of latency
-            $(`.tableBody2 tr:nth-child(${tabEvidence(imc)})`).addClass("categorie-en-evidence");
-        };
     });
-    /**
-     * this function treat the 'historique & supprimer-tout' button behavior
-     */
+
+    $("#enregistrer-button").on("click", function () {
+
+        let taille = $("#taille").val() / 100;
+        let poids = $("#poids").val();
+
+        let imc = calculerImc(taille, poids);
+
+        //increment local storage key position
+        i++;
+
+        localStorage.setItem(`IMC ${i}`, `Date: ${time()} / IMC: ${imc}`);
+    });
+
     $("#historique-button").on("click", function () {
 
-        //if else statment: to test if history is alrady shown
-        if ($(this).attr('data-click-state') == 1) {
+
+        if ($(this).attr('data-click-state') == 0) {
             // Set data-click-state attribute to 0
-            $(this).attr('data-click-state', 0);
+            $(this).attr('data-click-state', 1);
 
             //instruction to remove history from the web page if already existed
             $("#historique > *").remove();
             $("#supprimer-tout").remove();
 
-            if(localStorage.length <= 0) {
+            if (localStorage.length <= 0) {
                 alert("Aucun Historique");
+                $("#historique-button").attr('data-click-state', 0);
             };
             //generate supprimer-tout button with localStorage if data exist in local.Storage
             if (localStorage.length > 0) {
                 $("#historique").before(`<button id="supprimer-tout" type="button" class="btn btn-outline-primary">supprimer-tout</button>`);
             }
 
-            // supprimer-tout button behavior
             $("#supprimer-tout").on("click", function () {
                 //clear local storage
                 if (localStorage.length > 0) {
                     localStorage.clear();
                 }
-                
+
                 //to remove history from web
                 $("#historique > *").remove();
                 $("#supprimer-tout").remove();
-
+                location.reload();
                 // Make historique-button ready to for user click
                 $("#historique-button").attr('data-click-state', 1);
             });
-            //show Local.Storage data
-            allStorage().delay(800);
+
+            getAllStorage().delay(800);
         }
         else {
-            $(this).attr('data-click-state', 1);
-            //instruction to remove history from the web page
+            $(this).attr('data-click-state', 0);
+
             $("#historique > *").remove();
             $("#supprimer-tout").remove();
         }
